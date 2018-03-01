37e66ea3a8f09e57432aa9ec147069891c461fdf
RedOne <redouane.elghazi@gmail.com>
2018-03-01 20:03:32 +0100

    working version without output

--- Qualif/main.cpp
+++ Qualif/main.cpp
@@ -84,7 +84,7 @@ void getInput(const string& filename){
 		in >> rides[i];
 		remainingRides.insert(i);
 	}
-	
+
 	result.resize(F);
 
 }
@@ -97,7 +97,7 @@ int bestride(int v, int& newRelease){
     newRelease = T+1;
     Vehicle& V = vehicles[v];
     for(int r:remainingRides){
-        finish = max(V.releaseTime + d(V.x, V.y, rides[r].startX, rides[r].startY), rides[r].startTime) +
+        int finish = max(V.releaseTime + d(V.x, V.y, rides[r].startX, rides[r].startY), rides[r].startTime) +
             d(rides[r].startX, rides[r].startY, rides[r].endX, rides[r].endY);
         double new_obj;
         if(V.releaseTime + d(V.x, V.y, rides[r].startX, rides[r].startY)<=rides[r].startTime){
@@ -111,7 +111,7 @@ int bestride(int v, int& newRelease){
         if(finish <= rides[r].deadline and new_obj > obj){
             obj = new_obj;
             res = r;
-            new_Release = finish;
+            newRelease = finish;
         }
     }
     totalScore += newScore;
@@ -123,7 +123,7 @@ void findrides(){//maj remaining ride
 
 	priority_queue<pair<int,int> > q_vehicle; /* check si en place. Seems ok*/
 	for(int v = 0; v < F; ++v){
-		q_vehicle.push(make_pair(-vehicles[v].releaseTime,v));
+		q_vehicle.emplace(-vehicles[v].releaseTime,v);
 	}
 
 	int mini = 0;
@@ -140,7 +140,7 @@ void findrides(){//maj remaining ride
 			vehicles[veh_updated.second].x = rides[new_ride].endX;
 			vehicles[veh_updated.second].y = rides[new_ride].endY;
 			vehicles[veh_updated.second].releaseTime = new_time;
-			q_vehicle.push(-new_time,veh_updated.second);
+			q_vehicle.emplace(-new_time,veh_updated.second);
 
 			remainingRides.erase(new_ride);
 			result[veh_updated.second].push_back(new_ride);
