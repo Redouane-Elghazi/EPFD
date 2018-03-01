9cf5cafa04ec861729fe35ef15239143d8254bd9
RedOne <redouane.elghazi@gmail.com>
2018-03-01 19:06:53 +0100

    les fonctions centrales

--- Qualif/main.cpp
+++ Qualif/main.cpp
@@ -1,9 +1,17 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
+int bestride(int v){
+
+}
+
+void findrides(){
+
+}
 
 int main(int argc, const char * argv[]) {
-	
+
 	// insert code here...
 	std::cout << "Hello, World!\n";
     return 0;
-	
+
 }
