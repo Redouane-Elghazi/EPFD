521220863134f5c7558d85145dfaef938ad67b87
RedOne <redouane.elghazi@gmail.com>
2019-02-28 20:18:50 +0100

    rand

--- 2019/qualif_lyon.cpp
+++ 2019/qualif_lyon.cpp
@@ -14,44 +14,51 @@ istream& operator>>(istream& in, Photo& p){
 	return in;
 }
 
+ostream& operator<< (ostream& stream, const Slide& s){
+    cout << s.left->index;
+    if(s.right){
+        cout << " " << s.right->index;
+    }
+    cout << endl;
+    return stream;
+}
+
 vector<Slide> initial_random(vector<Photo>& photos){
     vector<Slide> ret;
-    vector<Photo> verts;
-    for(Photo x:photos){
+    vector<Photo*> verts;
+    for(Photo &x:photos){
         if(x.isHorizontal()){
             ret.emplace_back(&x);
         }
         else{
-            verts.pb(x);
+            verts.pb(&x);
         }
     }
     random_shuffle(verts.begin(), verts.end());
-    for(int i = 0; i+1<verts.size(); i+=2){
-        ret.emplace_back(&verts[i], &verts[i+1]);
+    for(unsigned int i = 0; i+1<verts.size(); i+=2){
+        ret.emplace_back(verts[i], verts[i+1]);
     }
     random_shuffle(ret.begin(), ret.end());
     return ret;
 }
 
+
 int main(){
 	cin.sync_with_stdio(0);
 	cin.tie(0);
-	srand(15);
+	srand(15456);
 	int n;
+	cin >> n;
 	vector<Photo> data(n);
 	FOR(i,n){
 		cin >> data[i];
 		data[i].index = i;
 	}
 
-	vector<Slide> output;
+	vector<Slide> output = initial_random(data);
 	cout << output.size() << endl;
 	for(Slide s : output){
-		cout << s.left->index;
-		if(s.right){
-			cout << " " << s.right->index;
-		}
-		cout << endl;
+        cout << s;
 	}
 	return 0;
 }
