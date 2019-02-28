5602bd62a2402ce08f3d0ad160037b3859e4da93
EnguerrandPrebet <enguerrand.prebet@ens-lyon.fr>
2019-02-28 19:06:40 +0100

    Create qualif_lyon.hpp
    Slide pas à jour.

---
+++ 2019/qualif_lyon.hpp
@@ -0,0 +1,12 @@
+#ifndef QUALIF_LYON_HPP_INCLUDED
+#define QUALIF_LYON_HPP_INCLUDED
+
+struct Photo{
+	char v_or_h;
+	int nb_tags;
+	vector<string> tags;
+};
+Slide = pair<int, pii>;
+
+
+#endif // QUALIF_LYON_HPP_INCLUDED
