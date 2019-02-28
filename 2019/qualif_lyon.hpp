068846a85d226a3843d6b513938306fa42432b58
Plopounet13 <lois.paulin@gmail.com>
2019-02-28 19:23:35 +0100

    Change Class Slide

--- 2019/qualif_lyon.hpp
+++ 2019/qualif_lyon.hpp
@@ -1,13 +1,37 @@
 #ifndef QUALIF_LYON_HPP_INCLUDED
 #define QUALIF_LYON_HPP_INCLUDED
 
+#include <vector>
+#include <string>
+
 struct Photo{
 	char v_or_h;
 	int nb_tags;
-	vector<string> tags;
+	std::vector<std::string> tags;
 	int index;
 };
-Slide = pair<Photo*, Photo*>;
+
+struct Slide{
+	Photo* left;
+	Photo* right;
+	
+	Slide(){
+		left = nullptr;
+		right = nullptr;
+	}
+	
+	Slide(Photo* l){
+		left = l;
+		right = nullptr;
+	}
+	
+	Slide(Photo* l, Photo* r){
+		left = l;
+		right = r;
+	}
+};
+
+int costInterSlide(Slide& s1, Slide& s2);
 
 
 #endif // QUALIF_LYON_HPP_INCLUDED
