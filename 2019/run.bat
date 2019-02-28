956abba538e0d85e53eb53beeac12018cbd29a0c
EnguerrandPrebet <enguerrand.prebet@ens-lyon.fr>
2019-02-28 21:10:10 +0100

    makefile win

--- 2019/run.bat
+++ 2019/run.bat
@@ -1,3 +1,4 @@
+g++ -std=c++11 -O3 -o qualif_lyon.exe qualif_lyon.cpp qualif_lyon.hpp slideshow.hpp slideshow.cpp
 qualif_lyon.exe < statement\a_example.txt > output\a.out
 qualif_lyon.exe < statement\b_lovely_landscapes.txt > output\b.out
 qualif_lyon.exe < statement\c_memorable_moments.txt > output\c.out
