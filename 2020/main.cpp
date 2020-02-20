623369ba0452c08d55906d29eb58862078a25b2a
Enguerrand Prebet <enguerrand.prebet@ens-lyon.fr>
2020-02-20 18:55:50 +0100

    data

--- 2020/main.cpp
+++ 2020/main.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
 
+using namespace std;
+
 int main() {
+
+    vector<vector<int>> librariesBooks;
+    vector<int> librariesDelay;
+    vector<int> librariesShipment;
+    vector<int> books;
+
+    int D, B, L;
+
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
