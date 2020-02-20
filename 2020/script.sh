81f2ecd24971e7aaef5c006e12ada119ba476c7a
RedOne <redouane.elghazi@gmail.com>
2020-02-20 21:58:33 +0100

    greedy is param

---
+++ 2020/script.sh
@@ -0,0 +1,11 @@
+make
+echo b
+time ./2020 < input/b_read_on.txt > output/b.out
+echo e
+time ./2020 < input/e_so_many_books.txt > output/e.out
+echo f
+time ./2020 < input/f_libraries_of_the_world.txt > output/f.out
+echo c
+time ./2020 < input/c_incunabula.txt > output/c.out
+echo d
+time ./2020 < input/d_tough_choices.txt > output/d.out
