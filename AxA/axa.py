5f7d2ff3108b06957ed5d807976a26ce7d20907b
RedOne <redouane.elghazi@gmail.com>
2020-04-29 17:07:44 +0200

    Update axa.py
    sorted wtf

--- AxA/axa.py
+++ AxA/axa.py
@@ -1,7 +1,7 @@
 import json
-from random import randint,seed
-seed(4568181913454)
-testcase = 6
+from random import randint,seed,shuffle
+seed(4948948561615)
+testcase = 3
 M = "morningOptions"
 E = "eveningOptions"
 D = "domain"
@@ -13,58 +13,63 @@ with open(infn, "r") as f:
 W = IN["workers"]
 quotas = IN["quotas"]
 td = dict()
-res = dict()
-for w in W:
-	res[w] = (min(range(len(W[w][M])), key=lambda m:sum(td[t]*(td[t]) if t in td else 0 for t in W[w][M][m])), min(range(len(W[w][E])), key=lambda e:sum(td[t]*(td[t]) if t in td else 0 for t in W[w][E][e])))
-	m,e = res[w]
-	for t in W[w][M][m]:
-		if t not in td:
-			td[t] = 0
-		td[t] += 1
-	for t in W[w][E][e]:
-		if t not in td:
-			td[t] = 0
-		td[t] += 1
+P = {w: [None, None] for w in W}
+workers = [(w,E) for w in list(W.keys())] + [(w,M) for w in list(W.keys())]
+workers.sort(key=lambda x:len(W[x[0]][x[1]]), reverse=True)
+for w, d in workers:
+	if d == M:
+		P[w][0] = min(range(len(W[w][M])), key=lambda m:sum(td[t]*(td[t]+1) for t in W[w][M][m] if t in td))
+		m = P[w][0]
+		for t in W[w][M][m]:
+			if t not in td:
+				td[t] = 0
+			td[t] += 1
+	else:
+		P[w][1] = min(range(len(W[w][E])), key=lambda e:sum(td[t]*(td[t]+1) for t in W[w][E][e] if t in td))
+		e = P[w][1]
+		for t in W[w][E][e]:
+			if t not in td:
+				td[t] = 0
+			td[t] += 1
 #res = {w:(min(range(len(W[w][M])), key=lambda m:sum(td[t]*(td[t]-1) for t in W[w][M][m])), min(range(len(W[w][E])), key=lambda e:sum(td[t]*(td[t]-1) for t in W[w][E][e]))) for w in W}
 
 T = dict()
 Q = dict()
-for w in res:
+for w in P:
 	if W[w][D] not in Q:
 		Q[W[w][D]] = 0
-	Q[W[w][D]] += 1
-	for t in W[w][M][res[w][0]]:
+	for t in W[w][M][P[w][0]]:
 		if t not in T:
 			T[t] = 0
-		T[t] += 1
-	for t in W[w][E][res[w][1]]:
+	for t in W[w][E][P[w][1]]:
 		if t not in T:
 			T[t] = 0
-		T[t] += 1
 	
+res = dict()
 while True:
-	print(len(res), end='\r')
-	rid = []
-	for w in res:
-		if Q[W[w][D]]>quotas[W[w][D]]:
-			S = 0
-			for t in W[w][M][res[w][0]]:
-				S += T[t]
-			for t in W[w][E][res[w][1]]:
-				S += T[t]
-			rid += [(S, w)]
-	if rid == []:
+	print(len(res),"/",sum(quotas[q] for q in quotas), end='\r')
+	need = []
+	for w in W:
+		if w not in res:
+			if Q[W[w][D]]<quotas[W[w][D]]:
+				S = 0
+				for t in W[w][M][P[w][0]]:
+					S += T[t]
+				for t in W[w][E][P[w][1]]:
+					S += T[t]
+				need += [(S, w)]
+	if need == []:
 		break
-	S, w = max(rid)
-	Q[W[w][D]] -= 1
-	for t in W[w][M][res[w][0]]:
-		T[t] -= 1
-	for t in W[w][E][res[w][1]]:
-		T[t] -= 1
-	del res[w]
+	S, w = min(need)
+	Q[W[w][D]] += 1
+	for t in W[w][M][P[w][0]]:
+		T[t] += 1
+	for t in W[w][E][P[w][1]]:
+		T[t] += 1
+	res[w] = P[w]
 	
 print("")
 print(sum(T[i]*(T[i]-1) for i in T))
-OUT = [{"name":	w, "morningOptionIndex": res[w][0], "eveningOptionIndex": res[w][1]} for w in res]
+OUT = [{"name":	w, "morningOptionIndex": P[w][0], "eveningOptionIndex": P[w][1]} for w in res]
 with open(outfn, 'w') as f:
 	json.dump(OUT, f)
