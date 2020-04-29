730b68f801dfed586048ace7d42c29e8745d3b5b
RedOne <redouane.elghazi@gmail.com>
2020-04-29 15:59:01 +0200

    dérandom

--- AxA/axa.py
+++ AxA/axa.py
@@ -1,7 +1,7 @@
 import json
 from random import randint,seed
 seed(4568181913454)
-testcase = 5
+testcase = 6
 M = "morningOptions"
 E = "eveningOptions"
 D = "domain"
@@ -12,7 +12,21 @@ with open(infn, "r") as f:
 	IN = json.load(f)
 W = IN["workers"]
 quotas = IN["quotas"]
-res = {w:(randint(0,len(W[w][M])-1), randint(0,len(W[w][E])-1)) for w in W}
+td = dict()
+res = dict()
+for w in W:
+	res[w] = (min(range(len(W[w][M])), key=lambda m:sum(td[t]*(td[t]) if t in td else 0 for t in W[w][M][m])), min(range(len(W[w][E])), key=lambda e:sum(td[t]*(td[t]) if t in td else 0 for t in W[w][E][e])))
+	m,e = res[w]
+	for t in W[w][M][m]:
+		if t not in td:
+			td[t] = 0
+		td[t] += 1
+	for t in W[w][E][e]:
+		if t not in td:
+			td[t] = 0
+		td[t] += 1
+#res = {w:(min(range(len(W[w][M])), key=lambda m:sum(td[t]*(td[t]-1) for t in W[w][M][m])), min(range(len(W[w][E])), key=lambda e:sum(td[t]*(td[t]-1) for t in W[w][E][e]))) for w in W}
+
 T = dict()
 Q = dict()
 for w in res:
@@ -49,7 +63,8 @@ while True:
 		T[t] -= 1
 	del res[w]
 	
-
+print("")
+print(sum(T[i]*(T[i]-1) for i in T))
 OUT = [{"name":	w, "morningOptionIndex": res[w][0], "eveningOptionIndex": res[w][1]} for w in res]
 with open(outfn, 'w') as f:
 	json.dump(OUT, f)
