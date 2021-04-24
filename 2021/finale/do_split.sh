c0c9a82ca2c2e8e66575fe68d32811063bbad8a2
RedOne <redouane.elghazi@gmail.com>
2021-04-24 22:08:21 +0200

    do do do the flop

---
+++ 2021/finale/do_split.sh
@@ -0,0 +1,4 @@
+python3 main2.py < input/constrained_optimisation.txt > output/c.txt
+python3 main2.py < input/distinction.txt > output/d.txt
+python3 main2.py < input/expectation_maximisation.txt > output/e.txt
+python3 main2.py < input/five_thousand.txt > output/f.txt
