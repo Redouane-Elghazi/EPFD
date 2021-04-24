c74b8b6b10ab1bd7bfc1da20865d8b7b2cfbfdc0
RedOne <redouane.elghazi@gmail.com>
2021-04-24 18:04:16 +0200

    Finale
    On prépare la finale

--- 2021/do_all.sh
+++
@@ -1,6 +0,0 @@
-python3 simulate.py < input/a.txt > output/a.out
-python3 simulate.py < input/b.txt > output/b.out
-python3 simulate.py < input/c.txt > output/c.out
-python3 simulate.py < input/d.txt > output/d.out
-python3 simulate.py < input/e.txt > output/e.out
-python3 simulate.py < input/f.txt > output/f.out
