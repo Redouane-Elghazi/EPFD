75981953495b58118874eddf4d2d205bbb25b17b
RedOne <redouane.elghazi@gmail.com>
2021-04-24 20:14:42 +0200

    v0.9 (tout le monde point enguerrand du doigt)

---
+++ 2021/finale/main.py
@@ -0,0 +1,78 @@
+from heapq import *
+L, G, S, B, F, N = [int(x) for x in input().split()]
+
+stoi = dict()
+itos = []
+btos = [[] for _ in range(B)]
+stob = []
+for s in range(S):
+	service, b = input().split()
+	b = int(b)
+	
+	stob += [b]
+	stoi[service] = s
+	itos += [service]
+	btos[b] += [s]
+
+ftoi = dict()
+itof = []
+ftos = [[] for _ in range(F)]
+D = []
+U = []
+for f in range(F):
+	feature, M, d, u = input().split()
+	M = int(M)
+	d = int(d)
+	u = int(u)
+	servs = input().split()
+	
+	ftoi[feature] = f
+	itof += [feature]
+	for s in servs:
+		ftos[f] += [stoi[s]]
+	ftos[f].sort(key=lambda s:-len(btos[stob[s]]))
+	D += [d]
+	U += [u]
+
+res = [[] for _ in range(G)]
+SF = []
+for f in range(F):
+	score = 0
+	cost = 0
+	bins = set()
+	for s in ftos[f]:
+		b = stob[s]
+		if b not in bins:
+			bins |= {b}
+			### TRY SUM
+			cost = max(cost, len(btos[b]))
+	cost += D[f]
+	### TRY SOMETHING ELSE 
+	score = U[f]/cost
+	SF += [(score, f)]
+
+SF.sort()
+events = []
+for g in range(G):
+	heappush(events, (0, g, None))
+
+btog = [0]*B
+for score, f in SF:
+	bins = set()
+	for s in ftos[f]:
+		b = stob[s]
+		if b not in bins:
+			bins |= {b}
+			t, g, rb = heappop(events)
+			if rb is not None:
+				btog[rb] -= 1
+			nt = t + D[f] + len(btos[b]) + btog[b]
+			heappush(events, (nt, g, b))
+			btog[b] += 1
+			res[g] += ["impl {} {}".format(itof[f], b)]
+
+res = [r for r in res if r!=[]]
+print(len(res))
+for r in res:
+	print(len(r))
+	print("\n".join(r))
