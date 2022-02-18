d9a39302d590ecae3fe8e232ba901fd7e06f03bc
RedOne <redouane.elghazi@gmail.com>
2022-02-18 12:00:38 +0100

    wtf on est en 2022 ?

---
+++ 2022/qualif/do_all.sh
@@ -0,0 +1,6 @@
+python3 main.py < input/a.txt > output/a.out
+python3 main.py < input/b.txt > output/b.out
+python3 main.py < input/c.txt > output/c.out
+python3 main.py < input/d.txt > output/d.out
+python3 main.py < input/e.txt > output/e.out
+python3 main.py < input/f.txt > output/f.out
