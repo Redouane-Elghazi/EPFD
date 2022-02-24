4100b72914c346608cd02d30c8ea52238386d022
RedOne <redouane.elghazi@gmail.com>
2022-02-24 19:07:53 +0100

    Update m_input.py

---
+++ 2022/qualif/m_input.py
@@ -0,0 +1,25 @@
+def read_input():
+	C = int(input())
+	P = int(input())
+	constributors, c2S = [], []
+	for c in range(C):
+		contributors += [input()]
+		N = int(input())
+		c2S += [dict()]
+		for i in range(N):
+			s, l = input().split()
+			c2S[-1][s] = int(l)
+	
+	projects, p2D, p2W, p2B, p2S = [], [], [], [], []
+	for p in range(P):
+		projects += [input()]
+		p2D += [int(input())]
+		p2W += [int(input())]
+		p2B += [int(input())]
+		r = int(input())
+		p2S += [dict()]
+		for i in range(r):
+			s, l = input().split()
+			p2S[-1][s] = int(l)
+		
+	return (contributors, c2S, projects, p2D, p2W, p2B, p2S)
