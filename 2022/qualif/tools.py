e2b2f9e80f26da535dbefa2de49cfbc4e1f2b856
Enguerrand Prebet <enguerrand.prebet@ens-lyon.fr>
2022-02-24 19:32:19 +0100

    body find_contrib

--- 2022/qualif/tools.py
+++ 2022/qualif/tools.py
@@ -1,13 +1,18 @@
 from typing import List, Dict, Tuple
 
-def learning(project : List[Tuple[str, int]], contributors : List[Dict[str, int]]) -> List[Dict[str, int]]:
-    new_contributors = [d for d in contributors]
-    n = len(project)
-    for i in range(n):
-        if contributors[i][project[0][i]] <= project[1][i]:
-            new_contributors[i][project[1][i]] += 1
-    return new_contributors
 
-def find_contributors(project, contributors, role):
-    #list of contributors filling the role, list of contributors filling the role if mentor
+# fait des copies
+def learning(project: List[Tuple[str, int]], contributors: List[Dict[str, int]]) -> List[Dict[str, int]]:
+	new_contributors = [d for d in contributors]
+	n = len(project)
+	for i in range(n):
+		if contributors[i][project[0][i]] <= project[1][i]:
+			new_contributors[i][project[1][i]] += 1
+	return new_contributors
 
+
+def find_contributors(project: List[Tuple[str, int]], contributors: List[Dict[str, int]], role: int):
+	no_mentor = [i for i in range(len(contributors)) if contributors[i][project[role][0]] >= project[role][1]]
+	with_mentor = [i for i in range(len(contributors)) if contributors[i][project[role][0]] == project[role][1]-1]
+
+	return no_mentor, with_mentor
