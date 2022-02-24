72e25d0d13bc2ff257cc5046adefa63fd479d366
Enguerrand Prebet <enguerrand.prebet@ens-lyon.fr>
2022-02-24 19:22:58 +0100

    body update learning

--- 2022/qualif/tools.py
+++ 2022/qualif/tools.py
@@ -1,8 +1,13 @@
-import typing
+from typing import List, Dict, Tuple
 
-def learning(project : Dict[str, int], contributors : List[Dict[str, int]]):
-    #contributors updated
-    return
+def learning(project : List[Tuple[str, int]], contributors : List[Dict[str, int]]) -> List[Dict[str, int]]:
+    new_contributors = [d for d in contributors]
+    n = len(project)
+    for i in range(n):
+        if contributors[i][project[0][i]] <= project[1][i]:
+            new_contributors[i][project[1][i]] += 1
+    return new_contributors
 
 def find_contributors(project, contributors, role):
     #list of contributors filling the role, list of contributors filling the role if mentor
+
