4041a041fa16c2605e4fd1e24b8ba083ba85eb2d
Enguerrand Prebet <enguerrand.prebet@ens-lyon.fr>
2022-02-24 20:24:28 +0100

    learning correction

--- 2022/qualif/tools.py
+++ 2022/qualif/tools.py
@@ -6,26 +6,27 @@ def learning(p2S: List[Tuple[str, int]], p2C_order: List[int], c2S: List[Dict[st
 	for i in range(n):
 		contrib = p2C_order[i]
 		role = p2S[i]
-		if c2S[contrib][role[0]] <= role[1]:
-			c2S[contrib][role[0]] += 1
+		if c2S[contrib].get(role[0], 0) <= role[1]:
+			c2S[contrib][role[0]] = c2S[contrib].get(role[0], 0) + 1
 
 
 def find_contributor(p2S: List[Tuple[str, int]], c2S: List[Dict[str, int]], role: int, mentor=False, start=0):
 	for i in range(start, len(c2S)):
-		if mentor and c2S[i][p2S[role][0]] >= p2S[role][1] - 1:
+		value = c2S[i].get(p2S[role][0], 0)
+		if mentor and value >= p2S[role][1] - 1:
 			return i
-		elif c2S[i][p2S[role][0]] >= p2S[role][1]:
+		elif value >= p2S[role][1]:
 			return i
 	return None
 
 
 def find_contributors(p2S: List[Tuple[str, int]], c2S: List[Dict[str, int]], role: int):
-	no_mentor = [i for i in range(len(c2S)) if c2S[i][p2S[role][0]] >= p2S[role][1]]
-	with_mentor = [i for i in range(len(c2S)) if c2S[i][p2S[role][0]] == p2S[role][1] - 1]
+	no_mentor = [i for i in range(len(c2S)) if c2S[i].get(p2S[role][0], 0) >= p2S[role][1]]
+	with_mentor = [i for i in range(len(c2S)) if c2S[i].get(p2S[role][0], 0) == p2S[role][1] - 1]
 
 	return no_mentor, with_mentor
 
-
+#value = c2S[i].get(p2S[role][0], 0)
 def find_contributors_yield(p2S: List[Tuple[str, int]], c2S: List[Dict[str, int]], role: int, mentor=False, start=0):
 	for i in range(start, len(c2S)):
 		if mentor and c2S[i][p2S[role][0]] >= p2S[role][1] - 1:
