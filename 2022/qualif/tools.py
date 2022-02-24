e401de7657c7a6c850776cb1699e1ff6cfac59d8
Enguerrand Prebet <enguerrand.prebet@ens-lyon.fr>
2022-02-24 19:10:17 +0100

    fonction à la con

---
+++ 2022/qualif/tools.py
@@ -0,0 +1,8 @@
+import typing
+
+def learning(project : Dict[str, int], contributors : List[Dict[str, int]]):
+    #contributors updated
+    return
+
+def find_contributors(project, contributors, role):
+    #list of contributors filling the role, list of contributors filling the role if mentor
