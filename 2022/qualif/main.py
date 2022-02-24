4fd87fb10649e0c502d8df964eb25d07e3a8f1b4
RedOne <redouane.elghazi@gmail.com>
2022-02-24 20:07:23 +0100

    Update main.py

--- 2022/qualif/main.py
+++ 2022/qualif/main.py
@@ -1,4 +1,43 @@
-import IO_hashcode
+from IO_hashcode import read_input, print_output
+from tools import find_contributors_yield
 import scoring
+from random import shuffle
 
-print(scoring.score())
+def attrib(p, i, res, used):
+	if i == len(p2S):
+		return True
+	for cont in find_contributors_yield(p2S[p], c2S, i, start=lastcont):
+		if cont not in used:
+			res += [cont]
+			used|={cont}
+			if attrib(p, i+1, res, used):
+				return True
+			res.pop(-1)
+			used-={cont}
+	return False
+
+contributors, c2S, projects, p2D, p2W, p2B, p2S = read_input()
+
+C = len(contributors)
+P = len(projects)
+
+last = -1
+cur = 0
+done = [False] * P
+done_projects = []
+p2c = []
+lastcont = 0
+while True:
+	if not(done[cur]):
+		res = []
+		if attrib(cur, 0, res, set()):
+			done[cur] = True
+			last = cur
+			lastcont = res[-1]
+			done_projects += [cur]
+			p2c += [res]
+	cur = (cur+1)%P
+	if last == cur:
+		break
+
+print_output(projects, contributors, done_projects, p2c)
